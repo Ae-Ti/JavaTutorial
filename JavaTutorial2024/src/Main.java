d7b37355e2a36380c3a52366c1c98c2547cd4063
Ae-Ti <taeilbae7@gmail.com>
2024-09-03 17:16:28 +0900

    variable is added in main file.

--- JavaTutorial2024/src/Main.java
+++ JavaTutorial2024/src/Main.java
@@ -1,7 +1,11 @@
 public class Main {
     public static void main(String[] args) {
 
+        String name;
+        name = "taeil";
         System.out.println("Hello world!");
         System.out.println("I like Java;");
+
+        System.out.println("My name is "+name+".");
     }
 }
